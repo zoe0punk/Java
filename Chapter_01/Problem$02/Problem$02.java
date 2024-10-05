23c3c088785914566a19af2424096362db9b4398
zoe0punk <151614338+zoe0punk@users.noreply.github.com>
2024-10-04 19:54:41 -0700

    Update Problem$02.java

--- Chapter_01/Problem$02/Problem$02.java
+++ Chapter_01/Problem$02/Problem$02.java
@@ -1,1 +1,13 @@
-
+/** Write a program display a message 5 times.
+     *@uthor : Trinh Ho
+    **/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Keep grinding you be successful one day.");
+		System.out.println("Keep grinding you be successful one day.");
+		System.out.println("Keep grinding you be successful one day.");
+		System.out.println("Keep grinding you be successful one day.");
+		System.out.println("Keep grinding you be successful one day.");
+	}
+}
