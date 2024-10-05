3a2023f23f8a13c3539c919a67bb7b33c494a511
zoe0punk <151614338+zoe0punk@users.noreply.github.com>
2024-10-04 19:49:36 -0700

    Update Problem$01.java

--- Chapter_01/Problem$01/Problem$01.java
+++ Chapter_01/Problem$01/Problem$01.java
@@ -1,1 +1,13 @@
+ /**
+* My first program in Java. Write a program display three message.
+*@author Trinh Ho
+**/
+public class Main
+{ 
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is ");
+	}
+}
 
