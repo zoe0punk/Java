6cce97de06e3bcf7a30f6b7d0a732f0d9a395a28
zoe0punk <151614338+zoe0punk@users.noreply.github.com>
2024-10-04 20:51:38 -0700

    Update Problem$02.java

--- Chapter_02/Problem$02.java
+++ Chapter_02/Problem$02.java
@@ -1,1 +1,18 @@
+/**
+     * Convert second to minute and remaining second.
+     *
+    **/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	Scanner scnr = new Scanner(System.in);
+	System.out.println("Enter an integer for seconds");
+	int seconds = scnr.nextInt();
+	int minute = seconds/60;
+	int remainingSeconds = seconds%60;
+	System.out.println(minute + " minutes and " + remainingSeconds + " seconds");
+	}
+}
 
